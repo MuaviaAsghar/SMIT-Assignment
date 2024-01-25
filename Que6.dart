c9d11b697a7a8bca7873633d995c828093ad9d7f
Muavia Asghar <newgamer445@gmail.com>
2024-01-25 23:49:44 +0500

    World

---
+++ Que6.dart
@@ -0,0 +1,11 @@
+void main() {
+  Map World = {
+    'USA': {'Capital': 'Washington', 'Currency': 'USD', 'Language': 'English'},
+    'India': {'Capital': 'New Delhi', 'Currency': 'INR', 'Language': 'Hindi'},
+    'Pakistan': {'Capital': 'Islamabad', 'Currency': 'PKR', 'Language': 'Urdu'},
+    'China': {'Capital': 'Beijing', 'Currency': 'CNY', 'Language': 'Chinese'},
+    'Russia': {'Capital': 'Moscow', 'Currency': 'RUB', 'Language': 'Russian'},
+    'Germany': {'Capital': 'Berlin', 'Currency': 'EUR', 'Language': 'German'},
+  };
+  print(World['China']);
+}
