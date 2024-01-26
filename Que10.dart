f93106b225b957227296956e2e8e4c530d134294
Muavia Asghar <newgamer445@gmail.com>
2024-01-26 20:18:06 +0500

    list

--- Que10.dart
+++ Que10.dart
@@ -1,5 +1,3 @@
-import 'test.dart';
-
 void main() {
   List<String> randstr = [
     'apple',
@@ -10,6 +8,6 @@ void main() {
     'grape'
   ];
   print("The list before removal of duplicates:$randstr");
-  var dup = removeDuplicates(randstr);
+  var dup = randstr.toSet().toList();
   print("The list after removal of duplicates:$dup");
 }
---
+++ Que11.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  List list = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20
+  ];
+  print("Enter any number ranging between 1 to 20:");
+  int? n = int.parse(stdin.readLineSync()!);
+  if (n <= list.length) {
+    for (int i = n; i <= n; i++) {
+      print(list.getRange(0, n));
+    }
+  } else {
+    print(list);
+  }
+}
