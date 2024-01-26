2bbf33a1c1dbf4d17fbf1623b35c8d682171cc06
Muavia Asghar <newgamer445@gmail.com>
2024-01-26 20:39:30 +0500

    reverse list

---
+++ Que12.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> list = ["M", "U", "A", "V", "I", "A"];
+  print("List before reversing$list");
+  List<String> reversedlist = list.reversed.toList();
+  print("List after reversing:$reversedlist");
+}
