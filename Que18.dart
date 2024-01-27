4b0fe6cfaee4b31b6c7981a4d18342a41de9e585
Muavia Asghar <newgamer445@gmail.com>
2024-01-27 18:01:58 +0500

    Minor changes 2

--- Que18.dart
+++ Que18.dart
@@ -1,7 +1,7 @@
 void main() {
   Map person = {"name": "John", "age": 18, "isStudent": true};
   if (person.containsKey('isStudent') &&
-      person.containsValue(true) &&
+      person["isStudent"] == true &&
       person.containsKey("age") &&
       person.containsValue(person["age"] >= 18)) {
     print("Eligible");
