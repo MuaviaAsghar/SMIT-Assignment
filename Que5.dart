76d2900f6283241411ebf8605d92092f2aa6b2b5
Muavia Asghar <newgamer445@gmail.com>
2024-01-31 19:21:37 +0500

    removed all the code

--- Que5.dart
+++ Que5.dart
@@ -1,13 +1,1 @@
-void main() {
-  Map dataofperson = {
-    'Name': 'Muavia',
-    'Age': '24',
-    'Phone Number': '+92 304 3217158',
-    'City': 'Karachi'
-  };
-  dataofperson.keys.forEach((key) {
-    if (key.length == 4) {
-      print(key);
-    }
-  });
-}
+
