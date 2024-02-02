9173605503618a89fb946609709699a823691e53
Muavia Asghar <newgamer445@gmail.com>
2024-02-02 12:56:06 +0500

    where

--- Que5.dart
+++ Que5.dart
@@ -1,1 +1,11 @@
+void main() {
+  Map<String, dynamic> map = {
+    "name": "Muavia",
+    "phone": 03449232398,
+  };
 
+  List<String> keysWithLength4 =
+      map.keys.where((key) => key.length == 4).toList();
+
+  print("Keys with length 4: $keysWithLength4");
+}
